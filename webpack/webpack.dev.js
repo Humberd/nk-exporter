6a73a526cf360cb6e27fcbf9971d9d9d5c2c55e6
humberd <humberd2@gmail.com>
2021-06-12 12:55:53 +0200

    Installed web-ext

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,7 +1,9 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+const WebExtPlugin = require('web-ext-plugin');
 
 module.exports = merge(common, {
     devtool: 'inline-source-map',
-    mode: 'development'
+    mode: 'development',
+    plugins: [new WebExtPlugin({ sourceDir: '../../dist', target: 'chromium' })]
 });
