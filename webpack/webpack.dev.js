1a928bf59615116d3a3aba48ed8aa967684dd66b
humberd <humberd2@gmail.com>
2021-06-12 13:14:14 +0200

    Chromium now persists test webpages

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,9 +1,19 @@
-const merge = require('webpack-merge');
-const common = require('./webpack.common.js');
-const WebExtPlugin = require('web-ext-plugin');
+const merge = require("webpack-merge");
+const common = require("./webpack.common.js");
+const WebExtPlugin = require("web-ext-plugin");
 
 module.exports = merge(common, {
-    devtool: 'inline-source-map',
-    mode: 'development',
-    plugins: [new WebExtPlugin({ sourceDir: '../../dist', target: 'chromium' })]
+  devtool: "inline-source-map",
+  mode: "development",
+  plugins: [
+    new WebExtPlugin({
+      sourceDir: "../../dist",
+      target: "chromium",
+      startUrl: "https://nk.pl",
+      browserConsole: true,
+      chromiumProfile: 'D://repositories/nk-exporter/chromium-test-profile',
+      profileCreateIfMissing: true,
+      keepProfileChanges: true
+    }),
+  ],
 });
